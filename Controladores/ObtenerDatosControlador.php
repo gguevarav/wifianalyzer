b9e3a030718629c02a9cba2ed86be81f10c22c2d
Daniel Guevara <gemisdguevarav@gmail.com>
2020-06-21 22:08:12 -0600

    Se modifica el controlador para que sea más eficiente, al montarlo en apache windows no ejecutaba las tareas por que tenia varios errores que en linux no los mostraba.

--- Controladores/ObtenerDatosControlador.php
+++ Controladores/ObtenerDatosControlador.php
@@ -1,6 +1,7 @@
 <?php
     class ObtenerDatosControlador {
-
+        
+        // GET: /api
         static public function mostrar(){
             $lecturas = self::obtenerDatos();
             // Preparamos el Json
@@ -10,11 +11,50 @@
                 "detalle" => $lecturas
             );
             header('Content-Type: application/json');
-            header('Access-Control-Allow-Origin: http://localhost/');
+            //header('Access-Control-Allow-Origin: http://localhost/');
             header('Access-Control-Allow-Methods: GET, POST');
             header("Access-Control-Allow-Headers: X-Requested-With");
             echo json_encode($json);
         }
+
+        // GET: /intensidades
+        static public function datosChart(){
+            // Obtenemos la data
+            $lecturas = self::obtenerDatos();
+            // Creamos el array
+            $arrayTiemposdBm = array();
+            // Agregaremos todas las tomas de los dBm a un array con su respectiva hora de toma
+			if(count($lecturas) < 20){
+				$inicio = count($lecturas);
+			}
+			else{
+				$inicio = count($lecturas)-20;
+			}
+			$final = count($lecturas);
+            for($contador = $inicio; $contador < $final; $contador++){
+                $arrayTemporal = array(
+                                          "id" => $lecturas[$contador]["id"],
+                                          "hora" => $lecturas[$contador]["hora"],
+                                          "intensidad" => $lecturas[$contador]["SignalLevel"]
+                );
+                // Agregamos el objeto al array principal
+                array_push($arrayTiemposdBm, $arrayTemporal);
+            }
+            // Preparamos el Json
+            $json = array(
+                "status" => 200,
+                "cantidad_datos" => count($arrayTiemposdBm),
+                "detalle" => $arrayTiemposdBm
+            );
+            // Devolvemos ldo datos
+            header('Content-Type: application/json');
+            //header('Access-Control-Allow-Origin: http://localhost/');
+            //header('Access-Control-Allow-Methods: GET, POST');
+            //header("Access-Control-Allow-Headers: X-Requested-With");
+            echo json_encode($json);
+        }
+
+        // Funcion principal
         static public function obtenerDatos(){
             // Abrimos el archivo en modo lectura
             $archivo = fopen("/home/drkprdx/Documentos/sitios/wifianalyzer/logs.txt", "r");
@@ -39,7 +79,6 @@
                 fclose($archivo);
                 // Mostramos el archivo
 
-
                 // Array que contendra array's anidados (objetos leidos)
                 $informacionLecturas = array();
 
@@ -48,110 +87,87 @@
                 for($contador = 0; $contador<count($arrayLineas); $contador++){
                     // Se crea un array temporal para agregar todas las linea que pertenecen al mismo objeto
                     $arrayTemporal = array();
-                    // Validamos si la linea que estamos leyendo es un digito, porque de ser asi significa que
-                    // es el inicio de un objeto
-                    if(ctype_digit(trim($arrayLineas[$contador]))){
-                        // Contador temporal que accedera a la siguiente linea, la actual es la del digito
-                        $contadorTemporal = $contador+1;
-                        // Mientras la linea no sea un digito, significa que pertenece al objeto, por lo tanto
-                        // se agrega al array temporal
-                        while(ctype_digit(trim($arrayLineas[$contadorTemporal])) == false && $contadorTemporal <= count($arrayLineas)){
-                            array_push($arrayTemporal, trim($arrayLineas[$contadorTemporal]));
-                            $contadorTemporal++;
+                    // Usaremos un ciclo for que leerá las líneas que corresponden a cada lectura que se encuentra en el archivo
+                    $contadorLinea = $contador;
+                    // El final del ciclo corresponde a la posición actual más 12 líneas que corresponde a la cantidad de
+                    // líneas que tiene cada lectura del archivo
+                    $fin = $contador+12;
+                    // Primero revisamos que no vayamos a leer fuera del indice del array que contiene todas las líneas
+                    if ($fin < count($arrayLineas)){
+                        // Iniciamos el ciclo for
+                        for($contadorLinea; $contadorLinea < $fin; $contadorLinea++){
+                            // Las veces que se repita, almacenará la línea leida en un array temporal
+                            array_push($arrayTemporal, trim($arrayLineas[$contadorLinea]));
                         }
-                        // Actualizamos el contador por que ya analizamos las lineas
-                        $contador = $contadorTemporal-1;
+                        // a su vez al salir de leer las líneas lo agrega al array principal que contendrá las lecturas individuales
+                        array_push($informacionLecturas, $arrayTemporal);
                     }
-                    // Agregamos el array del objeto al array que contendra todos los objetos
-                    array_push($informacionLecturas, $arrayTemporal);
+                    // Movemos el contador a la posición de donde toca extraer la información.
+                    $contador = $fin -1;
                 }
 
                 // Objetos de Lecturas
                 $lecturas = array();
 
                 // Bloque de código para almacenar la información obtenida en los arrays en objetos
-                for($Contador = 0; $Contador<count($informacionLecturas)-1; $Contador++){
+                for($Contador = 0; $Contador<count($informacionLecturas); $Contador++){
                     // Dividimos en tokens la informacion obtenida de la linea para luego almacenarla en el objeto
                     $arrayTokensLinea = array(1 => self::multiexplode(array(","," ","|", "\"", "="), $informacionLecturas[$Contador][0]),
                                               2 => self::multiexplode(array(","," ","|", "\"", "="), $informacionLecturas[$Contador][1]),
-                                            3 => self::multiexplode(array(","," ","|",":", "\"", "="), $informacionLecturas[$Contador][2]),
-                                            4 => self::multiexplode(array(","," ","|",":", "\"", "="), $informacionLecturas[$Contador][3]),
-                                            5 => self::multiexplode(array(","," ","|",":", "\"", "="), $informacionLecturas[$Contador][4]),
-                                            6 => self::multiexplode(array(","," ","|",":", "\"", "="), $informacionLecturas[$Contador][5]),
-                                            7 => self::multiexplode(array(","," ","|",":", "\"", "="), $informacionLecturas[$Contador][6]),
-                                            8 => self::multiexplode(array(","," ","|",":", "\"", "="), $informacionLecturas[$Contador][7]),
-                                            9 => self::multiexplode(array(","," ","|",":", "\"", "="), $informacionLecturas[$Contador][8]),
-                                            10 => self::multiexplode(array(","," ","|",":", "\"", "="), $informacionLecturas[$Contador][9]));
+                                              3 => self::multiexplode(array(","," ","|",":", "\"", "="), $informacionLecturas[$Contador][2]),
+                                              4 => self::multiexplode(array(","," ","|",":", "\"", "="), $informacionLecturas[$Contador][3]),
+                                              5 => self::multiexplode(array(","," ","|",":", "\"", "="), $informacionLecturas[$Contador][4]),
+                                              6 => self::multiexplode(array(","," ","|",":", "\"", "="), $informacionLecturas[$Contador][5]),
+                                              7 => self::multiexplode(array(","," ","|",":", "\"", "="), $informacionLecturas[$Contador][6]),
+                                              8 => self::multiexplode(array(","," ","|",":", "\"", "="), $informacionLecturas[$Contador][7]),
+                                              9 => self::multiexplode(array(","," ","|",":", "\"", "="), $informacionLecturas[$Contador][8]),
+                                              10 => self::multiexplode(array(","," ","|",":", "\"", "="), $informacionLecturas[$Contador][9]),
+                                              11 => self::multiexplode(array(","," ","|",":", "\"", "="), $informacionLecturas[$Contador][10]));
+
                     // Almacenamos la información obtenida en el objeto creado
                     // Creamos el objeto
                     $NuevaLectura = array(
-                                            "id" => $Contador,
-                                            "fecha" =>  $arrayTokensLinea[1][0],
-                                            "hora" =>  $arrayTokensLinea[2][0],
-                                            "Tarjeta" => $arrayTokensLinea[3][0],
-                                            "IEEE" => $arrayTokensLinea[3][5],
-                                            "ESSID" => $arrayTokensLinea[3][9],
-                                            "Mode" => $arrayTokensLinea[4][1],
-                                            "Frequency" => $arrayTokensLinea[4][4],
-                                            "AccessPoint" => $arrayTokensLinea[4][10].":".
-                                                            $arrayTokensLinea[4][11].":".
-                                                            $arrayTokensLinea[4][14].":".
-                                                            $arrayTokensLinea[4][13].":".
-                                                            $arrayTokensLinea[4][14].":".
-                                                            $arrayTokensLinea[4][15],
-                                            "BitRate" => $arrayTokensLinea[5][2],
-                                            "Velocidad" => $arrayTokensLinea[5][3],
-                                            "TxPower" => $arrayTokensLinea[5][7],
-                                            "RetryShortLimit" => $arrayTokensLinea[6][3],
-                                            "RTSThr" => $arrayTokensLinea[6][8],
-                                            "FragmentThr" => $arrayTokensLinea[6][13],
-                                            "PowerManagement" => $arrayTokensLinea[7][2],
-                                            "LinkQuality" => $arrayTokensLinea[8][2],
-                                            "SignalLevel" => $arrayTokensLinea[8][6],
-                                            "RxInvalidNwid" => $arrayTokensLinea[9][3],
-                                            "RxInvalidCrypt" => $arrayTokensLinea[9][8],
-                                            "RxInvalidFrag" => $arrayTokensLinea[9][13],
-                                            "TxExcessiveRetries" => $arrayTokensLinea[10][3],
-                                            "InvalidMisc" => $arrayTokensLinea[10][7],
-                                            "MissedBeacon" => $arrayTokensLinea[10][12]);
+                                            "id" => $arrayTokensLinea[1][0],
+                                            "fecha" =>  $arrayTokensLinea[2][0],
+                                            "hora" =>  $arrayTokensLinea[3][0],
+                                            "Tarjeta" => $arrayTokensLinea[4][0],
+                                            "IEEE" => $arrayTokensLinea[4][5],
+                                            "ESSID" => $arrayTokensLinea[4][9],
+                                            "Mode" => $arrayTokensLinea[5][1],
+                                            "Frequency" => $arrayTokensLinea[5][4],
+                                            "AccessPoint" => $arrayTokensLinea[5][10].":".
+                                                            $arrayTokensLinea[5][11].":".
+                                                            $arrayTokensLinea[5][14].":".
+                                                            $arrayTokensLinea[5][13].":".
+                                                            $arrayTokensLinea[5][14].":".
+                                                            $arrayTokensLinea[5][15],
+                                            "BitRate" => $arrayTokensLinea[6][2],
+                                            "Velocidad" => $arrayTokensLinea[6][3],
+                                            "TxPower" => $arrayTokensLinea[6][7],
+                                            "RetryShortLimit" => $arrayTokensLinea[7][3],
+                                            "RTSThr" => $arrayTokensLinea[7][8],
+                                            "FragmentThr" => $arrayTokensLinea[7][13],
+                                            "PowerManagement" => $arrayTokensLinea[8][2],
+                                            "LinkQuality" => $arrayTokensLinea[9][2],
+                                            "SignalLevel" => $arrayTokensLinea[9][6],
+                                            "RxInvalidNwid" => $arrayTokensLinea[10][3],
+                                            "RxInvalidCrypt" => $arrayTokensLinea[10][8],
+                                            "RxInvalidFrag" => $arrayTokensLinea[10][13],
+                                            "TxExcessiveRetries" => $arrayTokensLinea[11][3],
+                                            "InvalidMisc" => $arrayTokensLinea[11][7],
+                                            "MissedBeacon" => $arrayTokensLinea[11][12]);
 
                     // Agregamos el objeto al array principal
                     array_push($lecturas, $NuevaLectura);
                 }
 
             }
-            return $lecturas;
+
+            // Retornamos el array que contiene todas las lecturas con la información de forma individual
+			return $lecturas;
         }
 
-        // Mostramos los datos del primer chart
-        static public function datosChart(){
-            // Obtenemos la data
-            $lecturas = self::obtenerDatos();
-            // Creamos el array
-            $arrayTiemposdBm = array();
-            // Agregaremos todas las tomas de los dBm a un array con su respectiva hora de toma
-            for($contador = count($lecturas)-20; $contador < count($lecturas); $contador++){
-                $arrayTemporal = array(
-                                          "id" => $lecturas[$contador]["id"],
-                                          "hora" => $lecturas[$contador]["hora"],
-                                          "intensidad" => $lecturas[$contador]["SignalLevel"]
-                );
-                // Agregamos el objeto al array principal
-                array_push($arrayTiemposdBm, $arrayTemporal);
-            }
-            // Preparamos el Json
-            $json = array(
-                "status" => 200,
-                "cantidad_datos" => count($arrayTiemposdBm),
-                "detalle" => $arrayTiemposdBm
-            );
-            // Devolvemos ldo datos
-            header('Content-Type: application/json');
-            header('Access-Control-Allow-Origin: http://localhost/');
-            header('Access-Control-Allow-Methods: GET, POST');
-            header("Access-Control-Allow-Headers: X-Requested-With");
-            echo json_encode($json);
-        }
+        
 
         // Funcion para realizar un explode con varios delimitadores
         private static function multiexplode($delimitadores, $cadenaAnalizar) {
--- Rutas/Rutas.php
+++ Rutas/Rutas.php
@@ -9,7 +9,8 @@ class Rutas{
 		// Definimos las rutas
 		$RutasDefinidas = array("api",
 								"gui",
-								"intensidades");
+								"intensidades",
+								"canales");
 
 		// Array que contendrá la ruta en que estamos trabajando, se inicializa null por que se buscará en la uri para después hacer el debido desvío.
 		$InformacionURI = array("RutaUtilizar" => null,
@@ -73,6 +74,19 @@ class Rutas{
 					switch ($_SERVER["REQUEST_METHOD"]) {
 						case "GET":
 							header("Location: guiPrincipal.html");
+							break;
+					}
+				}
+			}
+			elseif ($InformacionURI["RutaUtilizar"] == "intensidades") {
+				// Si estamos recibiendo un verbo en la url vamos a verificar a cual lo redirigimos
+				if(isset($_SERVER["REQUEST_METHOD"])){
+					// Con un Switch bucaremos el verbo
+					switch ($_SERVER["REQUEST_METHOD"]) {
+						case "GET":
+							//echo "Hola Mundo";
+							$Mostrar = new ObtenerDatosControlador();
+							$Mostrar->datosChart();
 							break;
 					}
 				}
